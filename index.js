c2bb6bf05a1d9094c273329864a506ece2ad144a
Ashik465 <helloashikul@gmail.com>
2023-06-09 04:47:47 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,7 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 
+// verify jwt token
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if(!authorization) {
+    return   res.status(401).send({error:true ,message: "you are not authenticated"})
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err) {
+        return res.status(403).send({error:true ,message: "you are not authenticated"})
+      }
+      req.decoded = decoded
+      next()
+    })
+  }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vgwn8xr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -48,6 +64,15 @@ async function run() {
         res.send(result);
       });
  
+
+
+// create jwt token
+
+app.post('/jwt', (req, res) => {
+    const user = req.body 
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+    res.send({token})
+  })
 
 
 // class retaled api----------------
